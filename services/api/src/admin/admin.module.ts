182f96caa086c089a2ad0530b1ac7b7c99799fa6
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-08 23:38:21 +0100

    Fix admin module notifications dependency for orders service

--- services/api/src/admin/admin.module.ts
+++ services/api/src/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { OrdersModule } from '../orders/orders.module';
 import { ReportsModule } from '../reports/reports.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 import { AdminOrdersController } from './admin-orders.controller';
 import { AdminOrdersService } from './admin-orders.service';
@@ -10,25 +11,17 @@ import { AdminExportsService } from './admin.exports.service';
 import { AdminTownProductsService } from '../town-products/admin.town-products.service';
 import { AdminTownsController } from './admin.towns.controller';
 
-// existing
 import { AdminRefundsModule } from './refunds/admin.refunds.module';
-
-// product images admin endpoints
 import { AdminProductImagesModule } from './product-images/admin.product-images.module';
-
-// serve /admin/town-products
 import { AdminTownProductsController } from '../town-products/admin.town-products.controller';
-
-// variants controller
 import { AdminTownProductVariantsController } from '../town-products/admin.town-product-variants.controller';
-
-// NEW: drivers admin module
 import { AdminDriversModule } from './drivers/admin-drivers.module';
 
 @Module({
   imports: [
     OrdersModule,
     ReportsModule,
+    NotificationsModule,
     AdminRefundsModule,
     AdminProductImagesModule,
     AdminDriversModule,
@@ -36,10 +29,8 @@ import { AdminDriversModule } from './drivers/admin-drivers.module';
   controllers: [
     AdminOrdersController,
     AdminExportsController,
-
     AdminTownProductsController,
     AdminTownProductVariantsController,
-
     AdminTownsController,
   ],
   providers: [
