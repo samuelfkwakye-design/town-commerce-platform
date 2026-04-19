5105e81832c2e3c5df1a91e26c31512822bb0cb9
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-19 19:05:48 +0100

    fix: import AdminAuthModule in stock movements module

--- services/api/src/stock-movements/stock-movements.module.ts
+++ services/api/src/stock-movements/stock-movements.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { StockMovementsController } from './stock-movements.controller';
 import { StockMovementsService } from './stock-movements.service';
+import { AdminAuthModule } from '../admin-auth/admin-auth.module';
 
 @Module({
+  imports: [AdminAuthModule],
   controllers: [StockMovementsController],
   providers: [StockMovementsService, PrismaService],
 })
