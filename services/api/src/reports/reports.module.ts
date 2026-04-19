96492011ff3b254b09f1d0f7b4574347bdf54ec9
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-19 12:27:23 +0100

    fix: ensure all modules using AdminJwtGuard import AdminAuthModule

--- services/api/src/reports/reports.module.ts
+++ services/api/src/reports/reports.module.ts
@@ -3,8 +3,10 @@ import { PrismaService } from '../prisma/prisma.service';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { AdminReportsController } from './admin.reports.controller';
+import { AdminAuthModule } from '../admin-auth/admin-auth.module';
 
 @Module({
+  imports: [AdminAuthModule],
   controllers: [ReportsController, AdminReportsController],
   providers: [PrismaService, ReportsService],
   exports: [ReportsService],
--- services/api/src/town-products/town-products.module.ts
+++ services/api/src/town-products/town-products.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AdminAuthModule } from '../admin-auth/admin-auth.module';
 
 @Module({
+  imports: [AdminAuthModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
