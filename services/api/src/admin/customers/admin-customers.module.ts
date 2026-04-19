595581702d24c6773b50b412e4782e0224a26451
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-19 12:34:50 +0100

    fix: import AdminAuthModule in admin customers module

--- services/api/src/admin/customers/admin-customers.module.ts
+++ services/api/src/admin/customers/admin-customers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminCustomersController } from './admin-customers.controller';
 import { AdminCustomersService } from './admin-customers.service';
+import { AdminAuthModule } from '../../admin-auth/admin-auth.module';
 
 @Module({
+  imports: [AdminAuthModule],
   controllers: [AdminCustomersController],
   providers: [AdminCustomersService],
 })
