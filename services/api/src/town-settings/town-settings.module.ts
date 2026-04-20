c4f35c179f12799d9e0e51933170102eb9843f42
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-20 23:09:12 +0100

    fix: migrate town settings to JWT auth and import AdminAuthModule

--- services/api/src/town-settings/town-settings.module.ts
+++ services/api/src/town-settings/town-settings.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TownSettingsController } from './town-settings.controller';
 import { TownSettingsService } from './town-settings.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AdminAuthModule } from '../admin-auth/admin-auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AdminAuthModule],
   controllers: [TownSettingsController],
   providers: [TownSettingsService],
   exports: [TownSettingsService],
