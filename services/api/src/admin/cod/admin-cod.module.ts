ef993b5a4599d5288000fb1ec570c085ddd2bb25
Samuel Forson Kwakye <samuelfkwakye@gmail.com>
2026-04-25 21:58:09 +0100

    fix: import admin auth module in COD module

--- services/api/src/admin/cod/admin-cod.module.ts
+++ services/api/src/admin/cod/admin-cod.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AdminAuthModule } from '../../admin-auth/admin-auth.module';
 import { AdminCodController } from './admin-cod.controller';
 import { AdminCodService } from './admin-cod.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AdminAuthModule],
   controllers: [AdminCodController],
   providers: [AdminCodService],
 })
